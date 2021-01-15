26c6509460a60e466dd156d189a223932de00e14
etfaghaoubeid <etfaghaoubeid@gmail.com>
2021-01-15 00:40:03 +0000

    create  car models

---
+++ models/car/index.js
@@ -0,0 +1,47 @@
+const {DataTypes} = require('sequelize')
+const db = require('../../utils/db');
+const Car = db.define('cars', {
+    id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey:true
+    },
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    year: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    fuel: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+module.exports = Car;
